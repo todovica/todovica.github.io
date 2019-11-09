6d3c8c60195d5afb681998197a3476185ad436e3
todovica <ana.todovic@gmail.com>
2019-11-09 20:26:59 +0100

    deploy

---
+++ precache-manifest.6bcbb508e1643d0381066b36f67c3fc8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05573c627cb2e80f751d8810749d38b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f7ef59ffe381a5b12c4",
+    "url": "/static/css/main.152e1063.chunk.css"
+  },
+  {
+    "revision": "47fc33e26d1ce8b5dc24",
+    "url": "/static/js/2.26b884c5.chunk.js"
+  },
+  {
+    "revision": "9f7ef59ffe381a5b12c4",
+    "url": "/static/js/main.b7239176.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "ced73e0124bccec3c335e3d280bb5f0d",
+    "url": "/static/media/avatar.ced73e01.jpg"
+  },
+  {
+    "revision": "28cab272045788c1716611a451308bc5",
+    "url": "/static/media/cvcard.28cab272.png"
+  },
+  {
+    "revision": "aed29f115690bf3caff55c8d942c0df9",
+    "url": "/static/media/dreamcatchercard.aed29f11.png"
+  },
+  {
+    "revision": "1d3094df7cc725e10eaa7decdc7d026f",
+    "url": "/static/media/githubcard.1d3094df.png"
+  },
+  {
+    "revision": "ef62adf4708514f18436250f6f8c5a82",
+    "url": "/static/media/linkedincard.ef62adf4.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
