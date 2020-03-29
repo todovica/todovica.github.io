512c21a1eb4b3692d1470c80013f01cdd559b1b6
Ana Todovic <ana.todovic@gmail.com>
2020-03-29 12:34:18 +0200

    change site to rnf

---
+++ precache-manifest.d6f74e0f2459449a269084b330bac0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f716f175205fb626f76b34568ac34b2c",
+    "url": "/rockandfriendsweb/index.html"
+  },
+  {
+    "revision": "06b299697b52945e5c91",
+    "url": "/rockandfriendsweb/static/css/main.0b3051b3.chunk.css"
+  },
+  {
+    "revision": "c627fe9fcf377d77ec47",
+    "url": "/rockandfriendsweb/static/js/2.c3b9d598.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rockandfriendsweb/static/js/2.c3b9d598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b299697b52945e5c91",
+    "url": "/rockandfriendsweb/static/js/main.c931a269.chunk.js"
+  },
+  {
+    "revision": "3364aff738ef7a30f755",
+    "url": "/rockandfriendsweb/static/js/runtime-main.ac7e2f74.js"
+  },
+  {
+    "revision": "1b0f7a9b99d226269555cb27aeebbc73",
+    "url": "/rockandfriendsweb/static/media/logo.1b0f7a9b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfcde592409c620e2f3ddec13d17dce3.js"
+  "/rockandfriendsweb/precache-manifest.d6f74e0f2459449a269084b330bac0a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rockandfriendsweb/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
