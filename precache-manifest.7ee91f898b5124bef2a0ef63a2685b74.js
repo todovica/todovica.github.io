ff67397a63464f875d9148b3d1d4f0282a8af254
Ana Todovic <ana.todovic@gmail.com>
2020-03-29 17:39:20 +0200

    rnf again

---
+++ precache-manifest.7ee91f898b5124bef2a0ef63a2685b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28e8d223d6102abf9d1e22cb8a6e8b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9174bfaef9a9d362345e",
+    "url": "/static/css/main.6b8d5e3f.chunk.css"
+  },
+  {
+    "revision": "907d2eb20e0421100ffe",
+    "url": "/static/js/2.8c2def1e.chunk.js"
+  },
+  {
+    "revision": "9174bfaef9a9d362345e",
+    "url": "/static/js/main.9c43ee25.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "1b0f7a9b99d226269555cb27aeebbc73",
+    "url": "/static/media/logo.1b0f7a9b.png"
+  }
+]);
